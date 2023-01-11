638767871776729d73826dfee41ffb2e6a4e5a8b
Nane <nane.kds@fatoscorp.com>
2023-01-11 18:10:54 +0900

    first commit

---
+++ bin/dart_shelf_practice.dart
@@ -0,0 +1,5 @@
+import 'package:dart_shelf_practice/dart_shelf_practice.dart' as dart_shelf_practice;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_shelf_practice.calculate()}!');
+}
