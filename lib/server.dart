4c59bf3e5eb5f8d36e1549594ec8e49641e6202f
NaneNoiii <kds.napha@gmail.com>
2023-01-16 18:24:42 +0900

    comments

--- lib/server.dart
+++ lib/server.dart
@@ -1,10 +1,10 @@
-//External
+//External packages
 export 'package:shelf/shelf.dart';
 export 'package:shelf/shelf_io.dart';
 export 'dart:io' show Platform;
 export 'package:shelf_router/shelf_router.dart';
 export 'package:get_it/get_it.dart';
-//Internal
+//Internal packages
 export 'src/utils/get_it.dart';
 //Router
 export 'src/api/route/api_router.dart';
